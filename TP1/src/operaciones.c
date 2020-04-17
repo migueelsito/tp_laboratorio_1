6c53b7b0167b8d8e157ce3a49cfac20fcc5e3738
migueelsito <60529513+migueelsito@users.noreply.github.com>
2020-04-17 20:33:33 -0300

    Add files via upload

---
+++ TP1/src/operaciones.c
@@ -0,0 +1,60 @@
+/*
+ * utn.c
+ *
+ *  Created on: 11 abr. 2020
+ *      Author: Gil Miguel Ángel
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utn.h"
+
+float sumar(float a, float b)
+{
+	float resultado;
+	resultado=a+b;
+	return resultado;
+}
+
+float restar(float a,float b)
+{
+	float resultado;
+	resultado=a-b;
+	return resultado;
+}
+
+float multiplicar(float a,float b)
+{
+	float resultado;
+	resultado=a*b;
+	return resultado;
+}
+
+float dividir(float a,float b)
+{
+	float resultado;
+	if(a==0||b==0)
+	{
+		printf("No se puede dividir por 0");
+		exit(0);
+	}
+	resultado=a/b;
+	return resultado;
+}
+
+int factorizar(int a)
+{
+	int entero;
+	int resultado;
+	int i;
+
+	resultado=1;
+
+	entero=(int)a;
+	for(i=1; i<entero+1; i++){
+			resultado=resultado*i;
+	}
+	return resultado;
+}
+
+
